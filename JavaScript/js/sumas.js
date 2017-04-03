49c8aaad0b14da2e2b9f3edcd0a3e4cb567102fb
StephanyBacelis <sttephyblz@hotmail.com>
2017-04-03 15:01:07 -0600

    JS

---
+++ JavaScript/js/sumas.js
@@ -0,0 +1,18 @@
+function suma(a,b){
+  return a+b;
+}
+
+var suma2 = function (a,b) {
+  return a+b;
+}
+
+function texto() {
+  alert(document.getElementById("texto1").value)
+}
+
+function suma3(){
+  var a = document.getElementById("a").value;
+  var b = document.getElementById("b").value;
+  
+  alert(parseInt(a)+parseInt(b));
+}
