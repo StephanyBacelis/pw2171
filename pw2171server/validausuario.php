21dba43ae92bc86cab95a89479a759b9fdd4ecaf
StephanyBacelis <sttephyblz@hotmail.com>
2017-05-09 14:57:59 -0600

    Consulta

--- pw2171server/validausuario.php
+++ pw2171server/validausuario.php
@@ -1,4 +1,32 @@
 <?php
+  include('utilerias.php');
+
+  function validaUsuario($usuario,$clave){
+    //computadora, usuario, contraseña
+    $conexion=mysql_connect("localhost", "root", "");
+
+    mysql_select_db("pw2171");
+
+    $usuario=GetSQLValueString($usuario, "text");
+    $clave=GetSQLValueString(md5($clave), "text");
+
+    /*$consulta="select usuario, clave
+    from usuarios where usuario='".$usuario."'
+    and clave='".md5($clave)."' limit 1";*/
+    $consulta=sprintf("select usuario, clave
+      from usuarios where usuario=%s and clave=%s
+      limit 1", $usuario, $clave);
+
+    //--'1=1 <- true y puedes acceder a la bd.
+    $resultado = mysql_query($consulta);
+
+    if(mysql_num_rows($resultado)>0){
+      print("Bienvenido ".$usuario);
+    }else{
+      print("Bye.");
+    }
+
+  }
   //isset ->Si existe y trae un valor
   if(isset($_POST["txtUsuario"])&&isset($_POST["txtClave"])){
     //Arreglo de post o arreglo de get
@@ -7,8 +35,8 @@
     $usuario=$_POST["txtUsuario"];
     $clave = $_POST["txtClave"];
 
-    print($usuario);
-    print($clave);
+    validaUsuario($usuario,$clave);
+
   }else{
     print("<a href='acceso.html'>Valida tus datos</a>");
   }
